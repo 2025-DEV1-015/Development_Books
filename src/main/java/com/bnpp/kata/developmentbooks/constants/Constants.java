f0bbbfc6804b29b8b0bbed6eed83ea365649455a
2025-DEV1-015 <thangarajpradheep@gmail.com>
2026-03-20 12:01:04 +0530

    refactor: replace constant book list with enum and update service

--- src/main/java/com/bnpp/kata/developmentbooks/constants/Constants.java
+++ src/main/java/com/bnpp/kata/developmentbooks/constants/Constants.java
@@ -7,12 +7,4 @@ public class Constants {
     private Constants(){
 
     }
-
-    public static final List<String> BOOK_LIST = List.of(
-            "Clean Code",
-            "The Clean Coder",
-            "Clean Architecture",
-            "Test Driven Development by Example",
-            "Working Effectively With Legacy Code"
-    );
 }
--- src/main/java/com/bnpp/kata/developmentbooks/service/DevelopmentBooksService.java
+++ src/main/java/com/bnpp/kata/developmentbooks/service/DevelopmentBooksService.java
@@ -1,14 +1,16 @@
 package com.bnpp.kata.developmentbooks.service;
 
+import com.bnpp.kata.developmentbooks.constants.BookType;
 import com.bnpp.kata.developmentbooks.constants.Constants;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class DevelopmentBooksService {
 
     public List<String> getListOfBooks() {
-        return Constants.BOOK_LIST;
+        return Arrays.stream(BookType.values()).map(BookType::getTitle).toList();
     }
 }
