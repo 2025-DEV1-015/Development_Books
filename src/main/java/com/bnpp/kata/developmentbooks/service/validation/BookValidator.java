c27030c62a8f196177e501c3aa4b32991ad6aae5
2025-DEV1-015 <thangarajpradheep@gmail.com>
2026-03-20 17:12:29 +0530

    feat: implement BookValidator to check for null or invalid book titles

---
+++ src/main/java/com/bnpp/kata/developmentbooks/service/validation/BookValidator.java
@@ -0,0 +1,19 @@
+package com.bnpp.kata.developmentbooks.service.validation;
+
+import com.bnpp.kata.developmentbooks.model.BookItems;
+
+import java.util.List;
+import java.util.Objects;
+
+public class BookValidator {
+
+    public boolean validate(List<BookItems> bookItemsList) {
+
+        return bookItemsList.stream()
+                .anyMatch(item ->
+                        Objects.isNull(bookItemsList) ||
+                                Objects.isNull(item.getTitle()) ||
+                                item.getTitle().isBlank());
+
+    }
+}
---
+++ src/test/java/com/bnpp/kata/developmentbooks/service/validation/BookValidatorTest.java
@@ -0,0 +1,32 @@
+package com.bnpp.kata.developmentbooks.service.validation;
+
+import com.bnpp.kata.developmentbooks.model.BookItems;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BookValidatorTest {
+
+    private BookValidator bookValidator;
+
+    @BeforeEach
+    void setup() {
+        bookValidator = new BookValidator();
+    }
+
+    @Test
+    @DisplayName("BookValidator should return true when book title is empty")
+    void validateSingleBookWithEmptyData(){
+
+        List<BookItems> bookItemsList = Collections.singletonList(new BookItems("",1));
+        boolean hasInvalidData = bookValidator.validate(bookItemsList);
+
+        assertEquals(true,hasInvalidData);
+
+    }
+}
