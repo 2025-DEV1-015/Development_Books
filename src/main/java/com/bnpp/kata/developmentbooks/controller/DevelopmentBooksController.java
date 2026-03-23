be68e4267482ed7cdc4ccbbe14d2941050a91270
2025-DEV1-015 <thangarajpradheep@gmail.com>
2026-03-23 12:33:28 +0530

    feat: expose calculateBookPrice API for grouped pricing and discounts

--- src/main/java/com/bnpp/kata/developmentbooks/controller/DevelopmentBooksController.java
+++ src/main/java/com/bnpp/kata/developmentbooks/controller/DevelopmentBooksController.java
@@ -1,9 +1,12 @@
 package com.bnpp.kata.developmentbooks.controller;
 
+import com.bnpp.kata.developmentbooks.model.Books;
+import com.bnpp.kata.developmentbooks.model.OrderResponse;
 import com.bnpp.kata.developmentbooks.service.DevelopmentBooksService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +15,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/books")
 @RequiredArgsConstructor
-public class DevelopmentBooksController {
+public class DevelopmentBooksController implements CalculatePriceApi {
 
     private final DevelopmentBooksService developmentBooksService;
 
     @GetMapping("/getListOfBooks")
     public ResponseEntity<List<String>> getListOfBooks() {
         return ResponseEntity.ok(developmentBooksService.getListOfBooks());
+    }
+
+    @Override
+    public ResponseEntity<OrderResponse> calculateBookPriceApi(@RequestBody Books books) {
+        return ResponseEntity.ok(developmentBooksService.calculateBookPrice(books.getBooks()));
     }
 }
--- src/test/java/com/bnpp/kata/developmentbooks/controller/DevelopmentBooksControllerTest.java
+++ src/test/java/com/bnpp/kata/developmentbooks/controller/DevelopmentBooksControllerTest.java
@@ -1,19 +1,27 @@
 package com.bnpp.kata.developmentbooks.controller;
 
 import com.bnpp.kata.developmentbooks.constants.BookType;
+import com.bnpp.kata.developmentbooks.model.BookItems;
+import com.bnpp.kata.developmentbooks.model.Books;
+import com.bnpp.kata.developmentbooks.model.GroupDetails;
+import com.bnpp.kata.developmentbooks.model.OrderResponse;
 import com.bnpp.kata.developmentbooks.service.DevelopmentBooksService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -22,6 +30,9 @@ class DevelopmentBooksControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private DevelopmentBooksService developmentBooksService;
@@ -42,5 +53,43 @@ class DevelopmentBooksControllerTest {
                 .andExpect(jsonPath("$[2]").value("Clean Architecture"))
                 .andExpect(jsonPath("$[3]").value("Test Driven Development by Example"))
                 .andExpect(jsonPath("$[4]").value("Working Effectively With Legacy Code"));
+    }
+
+    @Test
+    @DisplayName("should return OrderResponse from service")
+    void shouldReturnDevelopmentBooksPrice() throws Exception {
+
+        Books books = new Books(List.of(
+                new BookItems("Clean Code", 2),
+                new BookItems("The Clean Coder", 2),
+                new BookItems("Clean Architecture", 2),
+                new BookItems("Test Driven Development by Example", 1),
+                new BookItems("Working Effectively With Legacy Code", 1)));
+
+        OrderResponse response = getOrderResponse();
+        Mockito.when(developmentBooksService.calculateBookPrice(Mockito.anyList()))
+                .thenReturn(response);
+
+        mockMvc.perform(post("/api/v1/books/calculatePrice")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(books)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.groups").isArray())
+                .andExpect(jsonPath("$.totalPrice").value(response.getTotalPrice()))
+                .andExpect(jsonPath("$.discountedPrice").value(response.getDiscountedPrice()));
+    }
+
+    private static OrderResponse getOrderResponse() {
+        List<String> books1 = Arrays.asList(
+                "Clean Code", "The Clean Coder", "Clean Architecture", "Test Driven Development by Example");
+        List<String> books2 = Arrays.asList(
+                "Clean Code", "The Clean Coder", "Clean Architecture", "Working Effectively With Legacy Code");
+
+        GroupDetails groupDetails1 = new GroupDetails(books1, 4, 20.0, 160.0);
+        GroupDetails groupDetails2 = new GroupDetails(books2, 4, 20.0, 160.0);
+
+        return new OrderResponse(List.of(groupDetails1, groupDetails2),
+                400.0, 320.0
+        );
     }
 }
